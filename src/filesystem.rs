bd87f3741ef7d0dfc65c5e44ae1989ff30587933
changyc9928 <changyc9928@gmail.com>
2022-08-02 13:46:09 +0800

    R21SC-68 Rename rename function as move

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -4,7 +4,7 @@ use std::io::Error;
 mod import_function {
     extern "C" {
         pub fn remove_file(ptr_to_path: u64, length_path: u64) -> u32;
-        pub fn rename_file(
+        pub fn move_file(
             ptr_to_new_path: u64,
             length_new_path: u64,
             ptr_to_old_path: u64,
@@ -28,8 +28,8 @@ pub unsafe fn remove_file(path: String) -> std::io::Result<()> {
     Ok(())
 }
 
-pub unsafe fn rename_file(path: String, new_path: String) -> std::io::Result<()> {
-    let ret = import_function::rename_file(
+pub unsafe fn move_file(path: String, new_path: String) -> std::io::Result<()> {
+    let ret = import_function::move_file(
         new_path.as_ptr() as u64,
         new_path.len() as u64,
         path.as_ptr() as u64,
