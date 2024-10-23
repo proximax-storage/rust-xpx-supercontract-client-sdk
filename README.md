9a634a8825b59be19721f5e43d738f373fbf5439
jackshenn <chanjackshen@gmail.com>
2024-10-23 16:51:23 +0800

    example

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Rust XPX Supercontract Client SDK
 
 This is a set of utils that can be useful to Supercontract developers
+
+## How to compile your rust code into wasm file
+1. Write your code in lib.rs
+2. run ```wasm-pack build```
+3. your wasm file can be find in ```/pkg``` folder
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,47 @@
+use std::io::{BufReader, Read};
+
+use blockchain::print_log;
+use serde::{Deserialize, Serialize};  // this is the external crate serde
+
 pub mod blockchain;
 pub mod dir_iterator;
 pub mod file;
 pub mod filesystem;
 pub mod internet;
+
+// here i use external crate for the struct to deserialize json into struct
+#[derive(Debug, Serialize, Deserialize)]
+struct Mosaic {
+    id: Vec<u32>,
+    amount: Vec<u32>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Account {
+    address: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Root {
+    account: Account,
+}
+
+// read rest endpoint, deserialize into struct, and print account address
+#[no_mangle]
+pub unsafe extern "C" fn check() -> u32 {
+
+    let baseurl = "http://127.0.0.1:3000/account/";
+    
+    // Read participants
+    {
+        let internet = internet::Internet::new(&format!("{}{}", baseurl, "SD2L2LRSBZUMYV2T34C4UXOIAAWX4TWQSQGBPMQO"), true).unwrap();
+        let mut reader = BufReader::with_capacity(1024, internet);
+        let mut internet_buffer = Vec::new();
+        reader.read_to_end(&mut internet_buffer).unwrap();
+        let result: Root = serde_json::from_slice(&internet_buffer).unwrap(); // here is also the serdejson 
+        
+        print_log(&result.account.address);
+    }
+    return 0;
+
+}
